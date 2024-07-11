0bd540530499a9e6d1e2ea948fefff7e431f86ba
ray <ray@fsr>
2024-07-11 16:55:44 +0800

    Chapter 14 Accessibility and form validation

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,6 +2,7 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter} from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 
 export default function Search({ placeholder }: { placeholder: string }) {
@@ -10,10 +11,13 @@ export default function Search({ placeholder }: { placeholder: string }) {
   const pathname = usePathname();
   const {replace} = useRouter();
 
-  function handleSearch(term: string){
+  // function handleSearch(term: string){
+    const handleSearch = useDebouncedCallback( (term) => {
     const params = new URLSearchParams(searchParams);
 
-    console.log(term);
+    console.log(`Searching ... ${term}`);
+    params.set('page', '1');
+    
     if (term){
       params.set('query', term);
     }
@@ -21,7 +25,8 @@ export default function Search({ placeholder }: { placeholder: string }) {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`)
-  }
+    console.log(params);
+  }, 500);
   
   
   
